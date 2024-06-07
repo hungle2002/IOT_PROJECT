aacfa0e5094b5e7c6125d37da58c0f5c500d78ad
Lê Quốc Hưng <98202931+hungle2002@users.noreply.github.com>
2024-06-07 21:59:42 +0700

    [Harry] Remove secret

--- Back_end/src/utils/httpRequestAda.ts
+++ Back_end/src/utils/httpRequestAda.ts
@@ -5,7 +5,7 @@ import {DrawFeedValue, FeedUpdateValue} from '../interfaces/ada';
 const request = axios.create({
   baseURL: 'https://io.adafruit.com/api/v2/heriota/',
   headers: {
-    'X-AIO-KEY': 'aio_PeJv96mtrfpDsKNLFfcmbP4KMiFk',
+    // 'X-AIO-KEY': 'aio_PeJv96mtrfpDsKNLFfcmbP4KMiFk',
   },
 });
 
